c4779a66f21ac4e748b857a41175ba54c5ebf2b2
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-26 02:08:26 +0100

    Add createBlog service

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,15 @@
-import axios from "axios";
+import axiosInstance from "../axiosInstance";
 
 const baseUrl = "/api/blogs";
 
 export const getBlogs = async () => {
-  const { data } = await axios.get(baseUrl);
+  const { data } = await axiosInstance.get(baseUrl);
+
+  return data;
+};
+
+export const createBlog = async (body) => {
+  const { data } = await axiosInstance.post(baseUrl, body);
 
   return data;
 };
