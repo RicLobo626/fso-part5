3472b21e81852cff3247886b1811d809d52f68a5
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-26 23:28:00 +0100

    Add and style blog details with expand functionality

--- src/components/UserView/BlogsSection.jsx
+++ src/components/UserView/BlogsSection.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import { Button } from "../Button";
+
 const Blog = ({ blog }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <li>
-      {blog.title} {blog.author}
+    <li className="blog">
+      <header className="blog-header">
+        <h3 className="blog-title">
+          {blog.title} - {blog.author}
+        </h3>
+
+        <Button
+          text={isExpanded ? "Hide" : "View"}
+          onClick={handleToggle}
+          aria-expanded={isExpanded}
+          aria-controls={`blog-${blog.id}-details`}
+        />
+      </header>
+
+      {isExpanded && (
+        <div id={`blog-${blog.id}-details`}>
+          <p>{blog.url}</p>
+          <p>
+            {blog.likes} likes <Button text="Like" />
+          </p>
+          <p>{blog.user.name}</p>
+        </div>
+      )}
     </li>
   );
 };
@@ -16,7 +46,7 @@ const Blogs = ({ blogs, isLoading }) => {
   }
 
   return (
-    <ul>
+    <ul className="blogs">
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
