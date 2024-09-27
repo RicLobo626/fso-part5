ef5debf3525473c4241ef407bc2693231a4c59f6
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-28 00:15:05 +0100

    Move BlogForm to dedicated folder

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,5 @@
 export { BlogFormSection } from "@/components/UserView/BlogFormSection/BlogFormSection";
-export { BlogForm } from "@/components/UserView/BlogFormSection/BlogForm";
+export { BlogForm } from "@/components/UserView/BlogFormSection/BlogForm/BlogForm";
 export { BlogsSection } from "@/components/UserView/BlogsSection/BlogsSection";
 export { Blog } from "@/components/UserView/BlogsSection/Blogs/Blog/Blog";
 export { Blogs } from "@/components/UserView/BlogsSection/Blogs/Blogs";
