71414ec5b33980d41aa3df9469fd7b23a3258a9c
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-27 20:39:13 +0100

    Config path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -10,6 +11,11 @@ export default defineConfig({
         target: "http://localhost:3002",
         changeOrigin: true,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
     },
   },
 });
