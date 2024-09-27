1ea322ea48b92a1e7c8a4620b7f9677b959562f4
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-27 23:43:21 +0100

    Remove unused import

--- src/components/UserView/BlogsSection/Blogs/Blog/Blog.test.jsx
+++ src/components/UserView/BlogsSection/Blogs/Blog/Blog.test.jsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Blog } from "@/components";
-import { beforeEach } from "vitest";
 
 describe("Blog component", () => {
   const blog = {
