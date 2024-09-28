1bc2bad071819aef2f1b0588185fd04467b6b540
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-28 20:36:28 +0100

    Add tests for login

--- bloglist-e2e/tests/bloglistApp.spec.js
+++ bloglist-e2e/tests/bloglistApp.spec.js
@@ -1,4 +1,7 @@
 const { test, describe, beforeEach, expect } = require("@playwright/test");
+const path = require("path");
+
+const authFile = path.join(__dirname, "../playwright/.auth/user.json");
 
 describe("Bloglist app", () => {
   beforeEach(async ({ page }) => {
@@ -13,5 +16,27 @@ describe("Bloglist app", () => {
     await expect(page.getByLabel("Username")).toBeVisible();
     await expect(page.getByLabel("Password")).toBeVisible();
     await expect(page.getByRole("button", { name: "Login" })).toBeVisible();
+  });
+
+  describe("Login", () => {
+    test("fails with wrong credentials", async ({ page }) => {
+      await page.getByLabel("Username").fill("testuser");
+      await page.getByLabel("Password").fill("wrongpassword");
+      await page.getByRole("button", { name: "Login" }).click();
+
+      await expect(page.getByRole("alert")).toHaveText(
+        "invalid username or password"
+      );
+    });
+
+    test("succeeds with correct credentials", async ({ page }) => {
+      await page.getByLabel("Username").fill("testuser");
+      await page.getByLabel("Password").fill("12345");
+      await page.getByRole("button", { name: "Login" }).click();
+
+      await expect(page.getByRole("alert")).toHaveText("Login successful");
+
+      await page.context().storageState({ path: authFile });
+    });
   });
 });
