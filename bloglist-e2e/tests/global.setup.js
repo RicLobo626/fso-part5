6daeddc26858c6b9e0a89a29204c1cd4195b4b52
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-28 22:14:45 +0100

    Move db logic to separate module and update global setup and teardown

--- bloglist-e2e/tests/global.setup.js
+++ bloglist-e2e/tests/global.setup.js
@@ -1,6 +1,8 @@
 const { test: teardown } = require("@playwright/test");
+const db = require("../db");
 
-teardown("create user", async ({ request }) => {
+teardown("setup db", async ({ request }) => {
+  await db.connect();
   await request.post("/api/users", {
     data: { username: "testuser", name: "Test User", password: "12345" },
   });
