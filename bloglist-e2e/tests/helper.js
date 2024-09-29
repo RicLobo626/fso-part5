61e65205073258e12ad2c46b2a2be7e5b5625bf5
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-29 02:23:35 +0100

    Add loginWith helper

--- bloglist-e2e/tests/helper.js
+++ bloglist-e2e/tests/helper.js
@@ -1,3 +1,9 @@
+const loginWith = async (page, username, password) => {
+  await page.getByLabel("Username").fill(username);
+  await page.getByLabel("Password").fill(password);
+  await page.getByRole("button", { name: "Login" }).click();
+};
+
 const createBlog = async (page, { title, author, url }) => {
   await page.getByRole("button", { name: "New blog" }).click();
   await page.getByLabel("Title").fill(title);
@@ -8,4 +14,4 @@ const createBlog = async (page, { title, author, url }) => {
   await page.getByRole("button", { name: "Cancel" }).click();
 };
 
-module.exports = { createBlog };
+module.exports = { loginWith, createBlog };
