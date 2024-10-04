9e94b9936a36f94b67290dc7535df6c6a6e24ece
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-05 00:23:32 +0100

    Move TheHeader to proper folder

--- bloglist-frontend/src/components/index.jsx
+++ bloglist-frontend/src/components/index.jsx
@@ -3,7 +3,7 @@ export { BlogForm } from "@/components/UserView/BlogFormSection/BlogForm/BlogFor
 export { BlogsSection } from "@/components/UserView/BlogsSection/BlogsSection";
 export { Blog } from "@/components/UserView/BlogsSection/Blogs/Blog/Blog";
 export { Blogs } from "@/components/UserView/BlogsSection/Blogs/Blogs";
-export { TheHeader } from "@/components/UserView/TheHeader";
+export { TheHeader } from "@/components/TheHeader";
 export { UserView } from "@/components/UserView/UserView";
 export { LoginForm } from "@/components/LoginForm";
 export { Notification } from "@/components/Notification";
