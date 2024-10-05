8bfc0546b29bbeb54d99dd32b720ffa0dc8467d9
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-05 10:10:53 +0100

    Remove section components for simplicity

--- bloglist-frontend/src/components/UserView/UserView.jsx
+++ bloglist-frontend/src/components/UserView/UserView.jsx
@@ -1,9 +1,27 @@
 import { useState } from "react";
-import { BlogFormSection, BlogsSection, Button } from "..";
+import { BlogForm, Blogs, Button } from "..";
 import PropTypes from "prop-types";
+import { handleError } from "@/helpers/errorHelper";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNotification } from "@/contexts/NotificationContext";
+import { createBlog } from "@/services/blogs";
 
 export const UserView = ({ user }) => {
   const [formIsVisible, setFormIsVisible] = useState(false);
+  const { showSuccess, showError } = useNotification();
+  const queryClient = useQueryClient();
+
+  const createBlogMutation = useMutation({
+    mutationFn: createBlog,
+    onSuccess: (blog) => {
+      showSuccess("Blog created successfully");
+      queryClient.setQueryData(["blogs"], (blogs) => blogs.concat(blog));
+    },
+    onError: (e) => {
+      const { message } = handleError(e);
+      showError(message);
+    },
+  });
 
   const handleToggleForm = () => {
     setFormIsVisible(!formIsVisible);
@@ -17,12 +35,18 @@ export const UserView = ({ user }) => {
         text="New blog"
       />
 
-      <BlogFormSection
-        onCancel={handleToggleForm}
-        className={!formIsVisible ? "hidden" : ""}
-      />
+      <section className={!formIsVisible ? "hidden" : ""}>
+        <h2>Create new</h2>
+        <BlogForm
+          onCreateBlog={createBlogMutation.mutate}
+          onCancel={handleToggleForm}
+        />
+      </section>
 
-      <BlogsSection user={user} />
+      <section>
+        <h2>All blogs</h2>
+        <Blogs user={user} />
+      </section>
     </main>
   );
 };
--- bloglist-frontend/src/components/index.jsx
+++ bloglist-frontend/src/components/index.jsx
@@ -1,8 +1,6 @@
-export { BlogFormSection } from "@/components/UserView/BlogFormSection/BlogFormSection";
-export { BlogForm } from "@/components/UserView/BlogFormSection/BlogForm/BlogForm";
-export { BlogsSection } from "@/components/UserView/BlogsSection/BlogsSection";
-export { Blog } from "@/components/UserView/BlogsSection/Blogs/Blog/Blog";
-export { Blogs } from "@/components/UserView/BlogsSection/Blogs/Blogs";
+export { BlogForm } from "@/components/UserView/BlogForm/BlogForm";
+export { Blog } from "@/components/UserView/Blogs/Blog/Blog";
+export { Blogs } from "@/components/UserView/Blogs/Blogs";
 export { TheHeader } from "@/components/TheHeader";
 export { UserView } from "@/components/UserView/UserView";
 export { LoginForm } from "@/components/LoginForm";
