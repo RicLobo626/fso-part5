c2fe7f12500e5361821ae4714f85e01bfa3547d0
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-06 18:48:35 +0100

    Style BlogList component

--- bloglist-frontend/src/components/BlogList.jsx
+++ bloglist-frontend/src/components/BlogList.jsx
@@ -1,12 +1,29 @@
+import clsx from "clsx";
+
 export const BlogList = ({ blogs }) => {
   if (blogs.length < 1) {
     return <p>No blogs available</p>;
   }
-
+  console.log(blogs[0]);
   return (
-    <ul>
-      {blogs.map((blog) => (
-        <li key={blog.id}>{blog.title}</li>
+    <ul className="rounded border">
+      {blogs.map((blog, i) => (
+        <li
+          key={blog.id}
+          className={clsx("flex flex-col p-2", {
+            "border-b": blogs.length - 1 !== i,
+          })}
+        >
+          <p>
+            <span className="font-medium">Title: </span>
+            {blog.title}
+          </p>
+
+          <p>
+            <span className="font-medium">Author: </span>
+            {blog.author}
+          </p>
+        </li>
       ))}
     </ul>
   );
