a5257a352b73f9d7672f4ed15d9642857a9e5def
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-06 18:08:37 +0100

    Add tailwind component for links

--- bloglist-frontend/src/components/Link.jsx
+++ bloglist-frontend/src/components/Link.jsx
@@ -2,7 +2,7 @@ import { Link as RouterLink } from "react-router-dom";
 
 export const Link = ({ text, ...props }) => {
   return (
-    <RouterLink className="text-sky-500" {...props}>
+    <RouterLink className="link" {...props}>
       {props.children || text}
     </RouterLink>
   );
