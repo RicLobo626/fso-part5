a0ac8eee06b0a5978734dd15bd03a8379a3e6b96
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-06 21:42:45 +0100

    Remove unused e2e project

--- bloglist-e2e/tests/global.teardown.js
+++
@@ -1,8 +0,0 @@
-const { test: teardown } = require("@playwright/test");
-const db = require("../db");
-
-teardown("cleanup db", async () => {
-  await db.resetBlogs();
-  await db.resetUsers();
-  await db.disconnect();
-});
