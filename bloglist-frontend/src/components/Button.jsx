7a1b37167e21c87427612dc1003ef0f4047972d2
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-06 13:39:09 +0100

    Style Button component

--- bloglist-frontend/src/components/Button.jsx
+++ bloglist-frontend/src/components/Button.jsx
@@ -1,7 +1,15 @@
 import PropTypes from "prop-types";
+import { Button as HUIButton } from "@headlessui/react";
 
 export const Button = ({ text, ...props }) => {
-  return <button {...props}>{text}</button>;
+  return (
+    <HUIButton
+      {...props}
+      className="bg-slate-200 border-slate-300 data-[hover]:bg-slate-300 border-2 rounded px-2"
+    >
+      {text}
+    </HUIButton>
+  );
 };
 
 Button.propTypes = {
