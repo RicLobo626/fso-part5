af1b9d5f47412a1b55b3d20159caef732198659f
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-06 15:40:23 +0100

    Update Button default classes

--- bloglist-frontend/src/components/Button.jsx
+++ bloglist-frontend/src/components/Button.jsx
@@ -2,11 +2,8 @@ import PropTypes from "prop-types";
 import { Button as HUIButton } from "@headlessui/react";
 
 export const Button = ({ text, className, ...props }) => {
-  const defaultClassName =
-    "bg-slate-200 border-slate-300 data-[hover]:bg-slate-300 border-2 rounded px-2";
-
   return (
-    <HUIButton {...props} className={`${defaultClassName} ${className}`}>
+    <HUIButton {...props} className={`btn ${className}`}>
       {text}
     </HUIButton>
   );
