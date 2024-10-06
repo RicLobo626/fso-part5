66f3c068c94d7ed8b1b730a05b0307d5687a5e7e
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-06 14:27:00 +0100

    Config Button to be styled from parent

--- bloglist-frontend/src/components/Button.jsx
+++ bloglist-frontend/src/components/Button.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 import { Button as HUIButton } from "@headlessui/react";
 
-export const Button = ({ text, ...props }) => {
+export const Button = ({ text, className, ...props }) => {
+  const defaultClassName =
+    "bg-slate-200 border-slate-300 data-[hover]:bg-slate-300 border-2 rounded px-2";
+
   return (
-    <HUIButton
-      {...props}
-      className="bg-slate-200 border-slate-300 data-[hover]:bg-slate-300 border-2 rounded px-2"
-    >
+    <HUIButton {...props} className={`${defaultClassName} ${className}`}>
       {text}
     </HUIButton>
   );
